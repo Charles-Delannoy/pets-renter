994c3db480d6bf061fcd1f155dcb59e81e963a91
Charles Delannoy <charles.delannoy@hei.fr>
2020-05-19 15:59:01 +0200

    show page created with routing and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :pets, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :pets, only: [:index, :new, :show, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
