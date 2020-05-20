44e86365a9ae9c21804877bf8d71b377cc3674eb
Charles Delannoy <charles.delannoy@hei.fr>
2020-05-20 16:16:19 +0200

    first seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroy bookings"
+Booking.destroy_all if Rails.env.development?
+puts "Destroy pets"
+Pet.destroy_all if Rails.env.development?
+puts "Destroy users"
+User.destroy_all if Rails.env.development?
+
+base_url = "https://source.unsplash.com/400x300/?"
+hm = ["men", "woman"]
+types = ["Chien", "Chat"]
+races_chien = ["labrador", "golden retriever", "bouvier", "bouledog"]
+races_chat = ["bengal", "burmilla", "korat", "munchkin"]
+noms = ["Riesling", "Tigger", "Max", "Charlie", "Rox", "Roukie", "Oliver", "Oreo", "Fluffy", "Jake", "Nurmi", "Bourgueil", "Catty", "Steve", "Leo"]
+
+
+i = 1
+j = 1
+10.times do
+  first = "firstname#{i}"
+  last = "lastname#{i}"
+  email = "mail#{i}@pet.com"
+  password = "lewagon"
+  sex = hm.sample
+  profile_url = base_url + sex
+  p_img = URI.open(profile_url)
+  @user = User.new(first_name: first, last_name: last, email: email, password: password)
+  @user.photo.attach(io: p_img, filename: "#{first}.png", content_type: 'image/png')
+  @user.save
+  i += 1
+  5.times do
+    type = types.sample
+    if type == "Chien"
+      race = races_chien.sample
+    else
+      race = races_chat.sample
+    end
+    pet_url = base_url + race
+    pet_img = URI.open(pet_url)
+    nom = noms.sample
+    birth = "2019-05-20"
+    address = "3 rue guynemer, Paris"
+    descr = "C'est un #{type} de race #{race} super mignon. Parfait pour chopper ;)"
+    @pet = Pet.new(name: nom, description: descr, birthdate: birth, pet_type: type, race: race, address: address)
+    @pet.user = @user
+    @pet.photo.attach(io: pet_img, filename: "#{nom}#{j}.png", content_type: 'image/png')
+    @pet.save
+    j += 1
+  end
+end
+
+puts "SUCCESS"
