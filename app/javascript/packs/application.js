ee92a73d765a1a36aba8babbc3c0345d3fd4cc69
Isabelle Lafont <isalafont@gmail.com>
2020-05-20 16:07:43 +0200

    Add map to pets/index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,15 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../plugins/init_mapbox';
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
 });
