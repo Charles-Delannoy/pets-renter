e944bf7d7e3aa80790283d32441ad1d1cabf8a6d
Isabelle Lafont <isalafont@gmail.com>
2020-05-21 18:03:48 +0200

    Map postion right

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 80, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 60, maxZoom: 14, duration: 0 });
 };
 const resize = (map) => {
   map.resize();
