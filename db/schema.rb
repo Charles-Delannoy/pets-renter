1e9e0f9023b52cb3fb4349bd0f8ce783c5f0576c
lena jakubowicz <lenajaku@gmail.com>
2020-05-21 14:50:20 +0200

    Add price to Pet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_130028) do
+ActiveRecord::Schema.define(version: 2020_05_21_124706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_130028) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.integer "price"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
