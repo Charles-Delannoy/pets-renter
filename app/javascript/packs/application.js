a7400be19053a7eafed8097bfcc4b32db163b83c
charles delannoy <charles.delannoy@hei.fr>
2021-07-15 15:13:40 +0200

    autocomplete address

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "../plugins/flatpickr"
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 
 document.addEventListener('turbolinks:load', () => {
-initMapbox();
+  initMapbox();
+  initAutocomplete();
   // Call your functions here, e.g:
   // initSelect2();
 });
